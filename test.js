2386a14915cea0d8179f893785cac6c37bc89ef1
Kevin Lindsey <kevin@kevlindev.com>
2016-10-17 14:10:53 -0700

    Disassemble some code in our test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-let Microprocessor = require('./lib/Microprocessor');
+let Microprocessor = require('./lib/Microprocessor'),
+	Disassembler = require('./lib/Disassembler');
 
 let memory = new Array(0x10000);
 let micro = new Microprocessor(memory);
@@ -19,3 +20,7 @@ console.log("before = %s", JSON.stringify(micro.registers, null, 2));
 // advance
 micro.advance();
 console.log("after  = %s", JSON.stringify(micro.registers, null, 2));
+
+let dasm = new Disassembler();
+let result = dasm.disassemble(memory, 0x600, 0x602);
+console.log("result = %s", JSON.stringify(result, null, 2));
